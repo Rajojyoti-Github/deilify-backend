6c7ce40ecfd936686873362a70b83db0118c39ec
Rajojyoti Banik <rajojyoti87@gmail.com>
2024-06-23 18:29:49 +0530

    order management commmit

---
+++ del-backend-ordermanagementservice/src/test/java/com/deilify/del_backend_ordermanagementservice/DelBackendOrdermanagementserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deilify.del_backend_ordermanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelBackendOrdermanagementserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
